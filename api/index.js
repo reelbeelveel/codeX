602fa8f36d0ba824b677ee5949231ae9baa77ba1
keelbeelveel <k.belveal17@gmail.com>
2020-07-13 21:14:47 -0400

    added snippet.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,9 @@
-// Script modified: Sun July 12, 2020 @ 05:37:38 EDT
+// Script modified: Mon July 13, 2020 @ 09:10:55 EDT
 const express = require('express');
 const app = express();
 
 //IMPORT ROUTES
-codeTypeRoute = require('./routes/api/codeTypes');
-app.use('/api/codeTypes', codeTypeRoute);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+const snippetRoute = require('./routes/api/snippet');
+app.use('/api/', snippetRoute);
 
 app.listen(3000);
---
+++ api/routes/api/snippet.js
@@ -0,0 +1,11 @@
+// Script modified: Mon July 13, 2020 @ 09:13:55 EDT
+const express = require('express');
+const router = express.Router();
+
+router.post('/', (req, res) => {
+});
+
+router.get('/:file', (req, res) => {
+});
+
+module.exports = router;
