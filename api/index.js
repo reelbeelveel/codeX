4b055b155333daa8d32fee6023840a482efcedcd
keelbeelveel <k.belveal17@gmail.com>
2020-07-14 17:53:43 -0400

    working express parameters!

--- api/index.js
+++ api/index.js
@@ -1,11 +1,11 @@
-// Script modified: Tue July 14, 2020 @ 04:36:46 EDT
+// Script modified: Tue July 14, 2020 @ 05:48:07 EDT
 const express = require('express');
 const app = express();
 
 //IMPORT ROUTES
 const apiCreateRouter = require('./routes/api/create');
 const apiViewRouter = require('./routes/api/view');
-app.use('/api/create*', apiCreateRouter);
-app.use('/api/view*', apiViewRouter);
+app.use('/api/create', apiCreateRouter);
+app.use('/api/view/:type/:reqId', apiViewRouter);
 
 app.listen(3000);
--- api/routes/api/create.js
+++ api/routes/api/create.js
@@ -1,28 +1,17 @@
-// Script modified: Tue July 14, 2020 @ 04:38:42 EDT
+// Script modified: Tue July 14, 2020 @ 05:53:14 EDT
 const express = require('express');
 const router = express.Router();
 const joi = require('@hapi/joi');
 
 
-router.all('/', (req, res) => {
-    console.log('api/create call');
-    res.send('received');
+router.get('/:type/:reqId', (req, res) => {
+    var type = req.params.type;
+    var reqId = req.params.reqId;
+    console.log(`Request of Type: ${type} and ID: ${reqId} received.`)
 });
 
 // TODO: Add joi schema for params?
 var typeScheme = joi.object({
     username: joi.string()
 });
-// TODO: Determine Parameter/Input Schema. (URL/body?)
-// TODO: Handle Requests!! ISSUE #2
-
-/*
-router.get('/create/:type', (req, res) => {
-});
-router.post('/create:params', (req, res) => {
-});
-// TODO: Build handler for requests coming from ISSUE #11
-// <Now ISSUE #14>
-router.get('/view', (req, res) => {
-});*/
 module.exports = router;
