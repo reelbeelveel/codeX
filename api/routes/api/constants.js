c5e3098dd3d33d9859362c7a1081df73d5edc227
keelbeelveel <k.belveal17@gmail.com>
2020-07-14 19:38:52 -0400

    working joi validation on api/create

---
+++ api/routes/api/constants.js
@@ -0,0 +1,6 @@
+// Script modified: Tue July 14, 2020 @ 07:20:15 EDT
+const tokenLength = 32;
+
+module.exports = {
+    tkLen: tokenLength
+};
--- api/routes/api/create.js
+++ api/routes/api/create.js
@@ -1,24 +1,30 @@
-// Script modified: Tue July 14, 2020 @ 06:22:01 EDT
+// Script modified: Tue July 14, 2020 @ 07:37:38 EDT
 const express = require('express');
 const router = express.Router();
 const joi = require('@hapi/joi');
 const constants = require('./constants');
+const tokenLength = constants.tkLen;
 
 // TODO: Add joi schema for params?
 const schema = joi.object({
     type: joi.string()
-        .alphanum()
-        .required(),
+    .alphanum()
+    .required(),
     reqId: joi.string()
-        .token()
-        .length(constants.tokenLength)
-        .required()
+    .token()
+    .length(tokenLength)
+    .required()
 });
-router.get('/:type/:reqId', (req, res) => {
-    
-    var type = req.params.type;
-    var reqId = req.params.reqId;
-    console.log(`Request of Type: ${type} and ID: ${reqId} received.`)
+
+router.get('/:type/:reqId', async (req, res) => {
+    try {
+        const value = await schema.validateAsync(req.params);
+        console.log("beep boop");
+        res.send(`Valid request for Type: ${value.type} and ID: ${value.reqId} received.`);
+    } catch (err) {
+        res.status(400).send(`Bad Request.\n${err}`).end();
+    }
 });
+
 
 module.exports = router;
