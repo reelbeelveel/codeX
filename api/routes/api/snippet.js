8a0e4f1c67bba3c5f14be33cb70d6b28817f71ca
keelbeelveel <k.belveal17@gmail.com>
2020-07-13 21:19:37 -0400

    added router.functions to match API Overview

--- api/routes/api/snippet.js
+++ api/routes/api/snippet.js
@@ -1,11 +1,17 @@
-// Script modified: Mon July 13, 2020 @ 09:13:55 EDT
+// Script modified: Mon July 13, 2020 @ 09:19:10 EDT
 const express = require('express');
 const router = express.Router();
 
-router.post('/', (req, res) => {
+/* REMOVE BEFORE MERGE -->
+ * API OVERVIEW MENTIONED IN COMMIT:
+ * https://gyazo.com/50bc1894160550986b1f734c29920499
+ */
+
+router.get('/create:params', (req, res) => {
+});
+router.post('/create:params', (req, res) => {
 });
 
-router.get('/:file', (req, res) => {
+router.get('/view', (req, res) => {
 });
-
 module.exports = router;
