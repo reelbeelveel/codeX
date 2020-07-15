e6b287760e2f84c803f6ca5d68035e37c528f3f1
keelbeelveel <k.belveal17@gmail.com>
2020-07-14 21:45:04 -0400

    working asynchronous validation

--- api/routes/api/create.js
+++ api/routes/api/create.js
@@ -1,11 +1,15 @@
-// Script modified: Tue July 14, 2020 @ 07:37:38 EDT
+// Script modified: Tue July 14, 2020 @ 09:44:40 EDT
 const express = require('express');
 const router = express.Router();
 const joi = require('@hapi/joi');
 const constants = require('./constants');
 const tokenLength = constants.tkLen;
 
-// TODO: Add joi schema for params?
+// JOI Validation Schema:
+//
+// - type:  Required alphanumeric string.
+// - reqID: Required aAzZ0123_ (Type: Token)
+//          string of len tokenLength.
 const schema = joi.object({
     type: joi.string()
     .alphanum()
@@ -16,11 +20,18 @@ const schema = joi.object({
     .required()
 });
 
+// Accepts request in the form url/api/create/TYPE/ID
 router.get('/:type/:reqId', async (req, res) => {
     try {
+        // Validates Type and ID using schema
         const value = await schema.validateAsync(req.params);
-        console.log("beep boop");
-        res.send(`Valid request for Type: ${value.type} and ID: ${value.reqId} received.`);
+        console.log(`Valid request for Type: ${value.type} and ID: ${value.reqId} received.`);
+
+        // TODO: Check "Cache"?
+        // TODO: If not in cache, submit to highlight engine.
+        // TODO: Return result to user.
+
+    // Handles Validation errors.
     } catch (err) {
         res.status(400).send(`Bad Request.\n${err}`).end();
     }
