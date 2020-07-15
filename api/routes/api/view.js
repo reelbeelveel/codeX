f595bc3f59ca04f60971c81dcf7646ed7964a9e2
keelbeelveel <k.belveal17@gmail.com>
2020-07-14 22:38:22 -0400

    added async joi validation. Route handler skeleton working.

--- api/routes/api/view.js
+++ api/routes/api/view.js
@@ -1,27 +1,38 @@
-// Script modified: Tue July 14, 2020 @ 04:38:33 EDT
+// Script modified: Tue July 14, 2020 @ 10:33:44 EDT
 const express = require('express');
 const router = express.Router();
 const joi = require('@hapi/joi');
+const constants = require('./constants');
+const snipIdLength = constants.snipLength;
 
-router.all('/', (req, res) => {
-    console.log('api/view call');
-    res.send('received');
+
+const idSchema = joi.object({
+    snipID: joi.string()
+    .alphanum()
+    .length(snipIdLength)
 });
 
-// TODO: Add joi schema for params?
-var typeScheme = joi.object({
-    username: joi.string()
-});
-// TODO: Determine Parameter/Input Schema. (URL/body?)
-// TODO: Handle Requests!! ISSUE #2
+router.get('/:snipID', async (req, res) => {
+    try {
+        const value = await idSchema.validateAsync(req.params);
+        if (value != {}){
+            console.log(`[/api/view.js] Valid GET request.`);
 
-/*
-router.get('/create/:type', (req, res) => {
+            // TODO: Find request from DB, return it.
+        } else {
+            console.log(`[/api/view.js] Valid GET request, no ID.`);
+
+            // TODO: Serve a stats page?
+            // Maybe most recent public snip? A feed? Idk.
+        }
+
+        // TODO: This is placeholder! Remove in future.
+        res.status(200)
+            .send("Hi! Your request has been received, but this part of the API is not yet functional.")
+            .end();
+    } catch (err) {
+        res.status(400).send(`Bad Request.\n${err}`).end();
+    }
 });
-router.post('/create:params', (req, res) => {
-});
-// TODO: Build handler for requests coming from ISSUE #11
-// <Now ISSUE #14>
-router.get('/view', (req, res) => {
-});*/
+
 module.exports = router;
