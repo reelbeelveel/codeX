69c3495b62bb2545fa1100ad8b85ed81dd7dd67d
keelbeelveel <k.belveal17@gmail.com>
2020-07-14 22:37:55 -0400

    cleaned up. Route handler skeleton working

--- api/routes/api/create.js
+++ api/routes/api/create.js
@@ -1,4 +1,4 @@
-// Script modified: Tue July 14, 2020 @ 09:44:40 EDT
+// Script modified: Tue July 14, 2020 @ 10:35:24 EDT
 const express = require('express');
 const router = express.Router();
 const joi = require('@hapi/joi');
@@ -25,13 +25,16 @@ router.get('/:type/:reqId', async (req, res) => {
     try {
         // Validates Type and ID using schema
         const value = await schema.validateAsync(req.params);
-        console.log(`Valid request for Type: ${value.type} and ID: ${value.reqId} received.`);
+        console.log(`[/api/create.js] Valid GET request for Type: ${value.type} and ID: ${value.reqId} received.`);
 
         // TODO: Check "Cache"?
         // TODO: If not in cache, submit to highlight engine.
         // TODO: Return result to user.
-
-    // Handles Validation errors.
+        // TODO: This is a placeholder! Remove in future.
+        res.status(200)
+            .send("Hi! Your request has been received, but this part of the API is not yet functional.")
+            .end();
+        // Handles Validation errors.
     } catch (err) {
         res.status(400).send(`Bad Request.\n${err}`).end();
     }
