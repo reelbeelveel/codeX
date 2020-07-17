ccf348edaa8bc15cf92433f8a06696cdf0dcd167
keelbeelveel <k.belveal17@gmail.com>
2020-07-17 12:48:56 -0400

    cleaned up scripts for centos8 server

---
+++ source/.sh/clean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Script modified: Mon July 06, 2020 @ 05:08:34 EDT
+echo " " 
+echo "######################"
+echo " clean.sh"
+echo "######################"
+echo " " 
+sudo rm -rv /var/www/cdX/html/*
+echo "  "
+echo "--------------------"
+echo "  "
+echo "/var/www/cdX Cleaned. Run ./build.sh to Rebuild."
+echo "  "
+echo "===================="
+echo "  "
