3af2af78ecb5f3fdded0c5c99323ed12084bfbb3
keelbeelveel <k.belveal17@gmail.com>
2020-07-29 00:23:27 -0400

    working, view

--- routes/view.js
+++ routes/view.js
@@ -1,4 +1,4 @@
-// Script modified: Tue July 28, 2020 @ 06:22:11 EDT
+// Script modified: Wed July 29, 2020 @ 12:22:36 EDT
 const express = require('express');
 const joi = require('@hapi/joi');
 const mysql = require('../sqlHandler');
@@ -6,25 +6,33 @@ const path = require('path');
 const router = express.Router();
 const tokenLength = 22;
 
-const idSchema = joi.object({
-    arg: joi.string()
-    .token()
-    .required(),
+const argSchema = joi.object({
     reqId: joi.string()
     .token()
     .length(tokenLength)
+    .required(),
+    arg: joi.string()
+    .token()
+    .required()
 });
-router.get('/', async (req, res) => {
+const idSchema = joi.object({
+    reqId: joi.string()
+    .token()
+    .length(tokenLength)
+    .required()
+})
+router.get('/:reqId', async (req, res) => {
     try {
+        const value = await idSchema.validateAsync(req.params);
         res.status(200).sendFile('view.html', { root: path.join(__dirname, '../pagesource/') });
     } catch (err) {
         res.status(400).send(`Bad Request. Error: ${err}`).end();
     }
 })
 
-router.get('/:arg/:reqId?', async (req, res) => {
+router.get('/:reqId/:arg', async (req, res) => {
     try {
-        const value = await idSchema.validateAsync(req.params);
+        const value = await argSchema.validateAsync(req.params);
         var fileName = undefined;
         if (value.reqId != {}){
                 console.log(`[/api/view.js] Valid GET request.`);
