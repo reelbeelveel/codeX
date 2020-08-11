998e55e9f2fd5d6a50e6ee0ac793bc766d85af12
keelbeelveel <k.belveal17@gmail.com>
2020-08-11 16:36:55 -0400

    more work on logger fixed fonts in snippets fixed screenshots (view.js)

--- logger.js
+++ logger.js
@@ -1,38 +1,42 @@
-// Script modified: Mon August 10, 2020 @ 08:38:29 EDT
+// Script modified: Tue August 11, 2020 @ 04:28:00 EDT
 const colors = require('colors');
 require('dotenv/config');
 var enabled = {
     info: process.env.LOG_INFO,
     debug: process.env.LOG_DEBUG,
+    verbose: process.env.LOG_VERBOSE,
     warn: process.env.LOG_WARN,
     error: process.env.LOG_ERROR
 };
 function log(data, level) {
     try{
-    var string;
-    if (data.match(/^>/) != null) {
-        string = `---> ${data.slice(1)}`.dim;
-    } else {
-        var timestamp = new Date().toISOString();
-        string = `\n[${timestamp}][${level}] ${data}`;
-    }
-    switch (level) {
-        case 'info':
-            console.log(string.dim);
-            break;
-        case 'debug':
-            console.log(string.white);
-            break;
-        case 'warn':
-            console.log(string.yellow.underline)
-            break;
-        case 'error':
-            console.log(string.red.bold);
-            break;
-    }
+        var string;
+        if (data.match(/^>/) != null) {
+            string = `---> ${data.slice(1)}`.dim;
+        } else {
+            var timestamp = new Date().toISOString();
+            string = `\n[${timestamp}][${level}] ${data}`;
+        }
+        switch (level) {
+            case 'info':
+                console.log(string.dim);
+                break;
+            case 'debug':
+                console.log(string.white);
+                break;
+            case 'verbose':
+                console.log(string.yellow);
+                break;
+            case 'warn':
+                console.log(string.yellow.underline)
+                break;
+            case 'error':
+                console.log(string.red.bold);
+                break;
+        }
     } catch (err) {
         console.log("LOGGING ERROR".red.bold.underline)
-    console.log(err.red)
+        console.log(err)
     }
 }
 
@@ -44,7 +48,9 @@ var logger = {
     debug: (data) => {
         if(enabled.debug) log(data, 'debug');
     },
-
+    verbose: (data) => {
+        if(enabled.verbose) log(data, 'verbose');
+    },
     warn: (data) => {
         if(enabled.warn) log(data, 'warn');
     },
--- routes/api/view.js
+++ routes/api/view.js
@@ -1,11 +1,13 @@
-// Script modified: Mon August 10, 2020 @ 12:57:51 EDT
+// Script modified: Tue August 11, 2020 @ 04:21:09 EDT
 const express = require('express');
 const fs = require('fs').promises;
 const joi = require('@hapi/joi');
+const logger = require('../../logger');
 const mysql = require('../../sqlHandler');
 const path = require('path');
 const puppeteer = require('puppeteer');
 const router = express.Router();
+const util = require('util');
 
 const tokenLength = 22;
 
@@ -19,15 +21,25 @@ const argSchema = joi.object({
 });
 
 router.get('/:reqId/:arg?', async (req, res) => {
+    logger.debug("[API/View] GET call to '/:reqId/:arg?'");
     try {
+        var content;
         const value = await argSchema.validateAsync(req.params);
-        if (value.arg == undefined) value.arg = 'img';
+        logger.debug('[API/View] Successfully validated schema.');
+
+        if (value.arg == undefined) {
+            logger.warn('[API/View] No argument provided.')
+            value.arg = 'img';
+        }
+
         var fileName = undefined;
-        console.log(`[/api/view.js] Valid GET request.`);
+
+        logger.debug(`> arg: ${value.arg}`);
+        logger.debug(`> reqId: ${value.reqId}`);
+
         switch(value.arg) {
-            case 'img': 
-                console.log(`    -> image call`);
-                // setup necessary sql queries, 
+            case 'img':
+                // setup necessary sql queries,
                 var query =
                     "SELECT style AS sheet, highlight " +
                     "FROM images i " +
@@ -35,72 +47,92 @@ router.get('/:reqId/:arg?', async (req, res) => {
                     "ON i.id = e.id " +
                     `WHERE i.id = '${value.reqId}' ` +
                     "LIMIT 2;";
-
+                logger.verbose('[API/View] Querying database');
+                logger.verbose(`> query: ${query}`);
                 var sql = await mysql.sqlQuery(query);
-                console.log(`    -> Query returned from database`);
+                logger.debug('[API/View] Result returned from database');
+                logger.debug(`> query: ${query}`);
+                logger.verbose(`> result: ${util.inspect(sql, false, null, true)}`);
+                console.log(util.inspect(sql, false, null, true));
+
                 const sheet = sql.result[0].sheet;
                 const highlight = sql.result[0].highlight;
-                console.log(`    -> loaded sheet: ${sheet}`);
-                console.log(`    -> loaded highlight length: ${highlight.length} `);
-                if (highlight == undefined || highlight == null || highlight == "") throw new Error(`No content defined for the identifier: ${value.reqId}`);
-                if (sheet == undefined || sheet == null || sheet == "") throw new Error(`No sheet defined for the identifier: ${value.reqId}`);
-                const browser = await puppeteer.launch({headless: true, args:['--no-sandbox']});
-                console.log('    -> opened puppeteer browser');
+                logger.debug(`> loaded sheet: ${sheet}`);
+                logger.debug(`> loaded highlight length: ${highlight.length} `);
+                logger.verbose(`>loaded highlight: ${util.inspect(highlight, false, null, true)}`);
+
+                if (highlight == undefined || highlight == null || highlight == "") {
+                    throw new Error(`No content defined for the identifier: ${value.reqId}`);
+                }
+                if (sheet == undefined || sheet == null || sheet == "") {
+                    throw new Error(`No sheet defined for the identifier: ${value.reqId}`);
+                }
+
+                const browser = await puppeteer.launch({headless: true, args:['--no-sandbox', '--font-render-hinting high']});
+                logger.debug('> (pup) Launched browser process.');
                 const page = await browser.newPage();
-                console.log('    -> launched new page');
+                logger.debug('> (pup) Opened new page.');
 
                 // Set our page's content (template as desired) + styleheet (from request, or default)?
-                let content = '<html class="hljs">' +
-                    '<pre id="bounding" style="margin: 0; padding: 0; position: fixed; top: 0; left: 0; font-size: larger;" class="hljs">' +
-                    '<code class="hljs" style="padding: 2em 2em 2em 2em; margin-bottom: 3em;">' +
+                content = '<html class="hljs">' +
+                    '<pre id="bounding" class="hljs">' +
+                    '<code class="hljs">' +
                     `${highlight}` +
-                    '</code><br style="padding: 2em;"><div id="watermark" style="display: block; background-color: #122a40; position: absolute; bottom: 8px; right: 8px; border: 2px solid #f78764; color: #b8336a; padding: 0.5em 1em 1.5em 0.5em; width: fit-content; height: fit-content; font-size: medium; border-radius: 20px;" align="right">' +
+                    '</code>' +
+                    '<br>' +
+                    '<div id="watermark" align="right">' +
                     '<img src="http://localhost:3000/Resources/openbracket.png" style="height: 2em; position: relative; transform: translate(0%, 35%);"/>' +
                     `codexapp.co/v?id=${value.reqId}` +
                     '<img src="http://localhost:3000/Resources/closebracket.png" style="height: 2em; position: relative; transform: translate(0%, 35%);"/>' +
                     '</div></pre></html>';
-                await page.setContent(content);
-                console.log('    -> loaded content');
+
+                await page.setContent(content, { waitUntil: "networkidle0" });
+                logger.debug('> (pup) Loaded content');
+
                 await page.addStyleTag({path: path.join(__dirname, `/../../pagesource/css/${sheet}`)});
-                console.log('    -> loaded stylesheet');
-                var { width, height } = await (await page.$('pre')).boundingBox();
-                if(width <= 800 && height <= 600) await page.setViewport({width: 800, height: 600});
-                else if(width <= 2000 && height <= 10000) await page.setViewport({ width: 2000, height: 10000 });
-                else {
-                    width = 2000;
-                    height = 10000;
-                    content = '<html class="hljs">' +
-                        '<div style="position: fixed:, top: 0; left: 0; background-color: red; color: white; font-size: x-large; font-weight: bold;">' +
-                        'ERROR: This output exceeds the current limit of 2000x10000px. It has been cropped to fit this limit. Consider breaking your code up into several snippets.' +
-                        '</div>' +
-                        '<pre id="bounding" style="margin: 0; padding: 0; position: fixed; top: 0; left: 0; font-size: larger;" class="hljs">' +
-                        '<code class="hljs" style="padding: 2em 2em 2em 2em;">' +
-                        `${highlight}` +
-                        '</code></pre><br style="padding: 2em;"><div id="watermark" style="background-color: #122a40; position: absolute; bottom: 8px; right: 8px; border: 2px solid #f78764; color: #b8336a; padding: 0.5em 1em 1.5em 0.5em; width: fit-content; height: fit-content; font-size: large; border-radius: 20px;" align="right">' +
-                        '<img src="http://localhost:3000/Resources/openbracket.png" style="height: 2em; position: relative; transform: translate(0%, 35%);"/>' +
-                        `codexapp.co/v?id=${value.reqId}` +
-                        '<img src="http://localhost:3000/Resources/closebracket.png" style="height: 2em; position: relative; transform: translate(0%, 35%);"/>' +
-                        '</div></html>';
-                    await page.setContent(content);
-                    await page.setViewport({ width: 2000, height: 10000 });
-                }
-                console.log('    -> viewport and watermark set successfully');
+                logger.debug(`> (pup) Loaded stylesheet ${sheet}`);
+
                 await page.addStyleTag({path: path.join(__dirname, '/../../pagesource/css/snippet.css')});
+                logger.debug('> (pup) snippet.css added');
+
+                await page.focus('pre#bounding');
+                logger.debug('> (pup) focused pre#bounding');
+
+                var { width, height } = await (await page.$('pre')).boundingBox();
+                width = Math.ceil(width);
+                height = Math.ceil(height);
+                logger.debug(`> (pup) Got dimensions: ${width} x ${height}`);
+
+
+                await page.setViewport({ width, height });
+                logger.debug('> (pup) viewport set');
+
+                try {
+                    const content = await page.content()
+                    logger.verbose(`> (pup) page content:\n${util.inspect(content, false, null, true)}`);
+                } catch (err) {
+                    logger.warn('[API/View] Error checking page content');
+                    logger.error(`> ${err}`);
+                }
                 await page.screenshot({path: path.join(__dirname, `/../../exports/${value.reqId}.png`), clip: { x: 0, y: 0, width, height}, defaultViewport: null});
-                console.log('    -> screenshot taken');
+                logger.debug('[API/View] Screenshot taken')
                 await page.close();
+                logger.verbose('> (pup) page closed');
                 await browser.close();
-                console.log('browser closed');
+                logger.verbose('> (pup) browser closed');
                 fileName = `${value.reqId}.png`;
                 break;
             default: 
-                throw new Error('Invalid args');
+                throw new Error('[API/View.js] Line 118: Invalid argument.');
 
         }
+
         if (fileName === undefined) throw new Error(`The specified ID ${value.reqId} did not produce a valid filename.`);
         res.status(200).sendFile(`${fileName}`, { root: path.join(__dirname, '../../exports')});
-        console.log('response sent');
+        logger.debug(`[API/View] Sent file ${fileName} in response to api view request`);
     } catch (err) {
+        logger.error("[API/View] Error handling request:");
+        logger.error(`> ${err}`);
         res.status(400).send(`Bad Request.\n${err}`).end();
     }
 });
